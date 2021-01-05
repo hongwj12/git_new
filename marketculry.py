c7754ee2f50cd556eccf466efe8c7995a0d12642
hongwj12 <hongwj12@snu.ac.kr>
2021-01-05 12:55:24 +0900

    브랜치 만들어써요

---
+++ marketculry.py
@@ -0,0 +1,11 @@
+like = True
+cart_number = [1, 2, 3, 4]
+
+
+def like():
+    if like == True:
+        if cart_num == True
+        cart_number[0] -= 1
+        print(f'hello')
+    else:
+        cart_number[1] += 1
